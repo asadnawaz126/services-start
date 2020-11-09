0562e5cb5f2fe6f0722b81be8f46cbbe7364014d
Asad Nawaz <asadnawazmobilecrazy@gmail.com>
2020-11-08 23:59:39 -0500

    made account service injectable and injected logging service into it

--- src/app/accounts.service.ts
+++ src/app/accounts.service.ts
@@ -1,3 +1,7 @@
+import {LoggingService} from './logging.service';
+import {Injectable} from '@angular/core';
+
+@Injectable() // tells angular that things can be injected into this service
 export class AccountsService {
   accounts = [
     {
@@ -14,11 +18,15 @@ export class AccountsService {
     }
   ];
 
+  constructor(private loggingService: LoggingService) {}
+
   addAccount(name: string, status: string) {
     this.accounts.push({name, status});
+    this.loggingService.logStatusChanged(status);
   }
 
   updateStatus(id: number, status: string) {
     this.accounts[id].status = status;
+    this.loggingService.logStatusChanged(status);
   }
 }
